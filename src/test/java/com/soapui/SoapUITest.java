cf9f2c21d68e7b96429b5e0bcd2408093fb528f0
BaneleMlamleli <mlamlelibanele@yahoo.com>
2024-02-27 17:11:33 +0200

    Added method to test individual test cases in SoaUI

--- src/test/java/com/soapui/SoapUITest.java
+++ src/test/java/com/soapui/SoapUITest.java
@@ -1,22 +1,23 @@
 package com.soapui;
 
-// import static org.junit.Assert.assertTrue;
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.model.iface.Submit.Status;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.*;
 import com.eviware.soapui.tools.SoapUITestCaseRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class SoapUITest {
 
     @Test
-    public void testRunner() throws Exception {
-        SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
-        runner.setProjectFile( "C:\\Users\\ciphe\\Documents\\Programming_local\\API\\payu_automation_assignment\\Automation Assignment.xml");
-        runner.run();
-    }
-
-    @Test
     public void testSoapUIIntegration() {
         // Path to your SoapUI project file
-        String projectPath = "C:\\Users\\ciphe\\Documents\\Programming_local\\API\\SoapUI\\CountryInfoService-soapui-project_proj.xml";
+        String projectPath = "src\\resource\\automation_assignment_project.xml";
 
         // Create a new instance of SoapUITestCaseRunner
         SoapUITestCaseRunner runner = new SoapUITestCaseRunner();
@@ -28,10 +29,21 @@ public class SoapUITest {
             // Run all test cases in the project
             runner.run();
 
-            // Check if all test cases passed
-            // assertTrue(runner.getResults().getErrorCount() == 0);
+            // Check if all failed cases
+            assertFalse(runner.getFailedTests().isEmpty());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testIndividualTestCaseRunner() throws Exception {
+        WsdlProject project = new WsdlProject( "src\\resource\\automation_assignment_project.xml" ); 
+        TestSuite testSuite = project.getTestSuiteByName( "Test Suite 2" ); 
+        TestCase testCase = testSuite.getTestCaseByName( "Test Case 1" );
+        
+        // create empty properties and run synchronously
+        TestRunner runner = testCase.run( new PropertiesMap(), false ); 
+        assertEquals(Status.FINISHED, runner.getStatus() ); 
     }
 }
